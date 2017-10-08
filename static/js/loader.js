7c64ed1163da4359c5b63e774f20d2e98f7fbec8
pH7s <phenrysoria@gmail.com>
2017-10-07 22:00:02 -0700

    Change libraries to https since GitHub Pages is now loaded under https

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,16 +1,16 @@
 /**
  * Author:          Pierre-Henry Soria <ph7software@gmail.com>
- * Copyright:       (c) 2013-2015, Pierre-Henry Soria. All Rights Reserved.
+ * Copyright:       (c) 2013-2017, Pierre-Henry Soria. All Rights Reserved.
  * License:         CC-BY - http://creativecommons.org/licenses/by/3.0/
  * Link:            http://github.com/pH-7
  */
 
 var aFileList = [
-    'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min',
+    'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min',
     './static/js/Git',
     './static/js/jquery.tipsy',
     './static/js/common',
-    'http://s7.addthis.com/js/250/addthis_widget'
+    'https://s7.addthis.com/js/250/addthis_widget'
 ];
 
 for(i in aFileList)
